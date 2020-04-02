3eb1c32d99af9cd0292147ec7577d916e2555991
Keiji Utida <leandro.keiji@gmail.com>
2020-04-01 23:31:42 -0300

    Added sales viewmodels

--- SalesWebMVC/Models/Departament.cs
+++ SalesWebMVC/Models/Departament.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace SalesWebMVC.Models
 {
@@ -6,5 +8,23 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+        public Departament() 
+        { 
+        }
+
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void AddSeler(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public double TotalSales(DateTime inicial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(inicial, final));
+        }
     }
 }
